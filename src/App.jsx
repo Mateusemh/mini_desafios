84451b1e8c91d99174286188c571f44bc1b58082
Mateusemh <mateus45augusto45@gmail.com>
2026-06-03 11:44:55 -0300

    Commita pls

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MensagemAoAbrir from './components/MensagemAoAbrir'
+import ListaProdutos from './components/ListaProdutos'
+import CadastraTarefa from './components/CadastraTarefa'
+import RemoveProduto from './components/RemoveProduto'
+
+const App = () => {
+    return (
+        <div>
+            {/* <MensagemAoAbrir/> */}
+            {/* <ListaProdutos /> */}
+            {/* <CadastraTarefa /> */}
+            <RemoveProduto />
+        </div>
+    )
+}
+
+export default App
